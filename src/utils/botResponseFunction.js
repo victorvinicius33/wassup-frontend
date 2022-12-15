bd28c6f3935571b4be5678ebdd236fcabfb060da
victorvinicius33 <victorvinicius33.vv@gmail.com>
2022-12-15 06:01:04 -0300

    change logic to compare in lowercase, the question and the answer of the bot

--- src/utils/botResponseFunction.js
+++ src/utils/botResponseFunction.js
@@ -2,7 +2,7 @@ const stringSimilarity = require('string-similarity');
 const botResponseData = require('./botResponseData');
 
 module.exports = (input) => {
-  const matches = stringSimilarity.findBestMatch(input, botResponseData.inputs);
+  const matches = stringSimilarity.findBestMatch(input.toLowerCase(), botResponseData.inputs);
   const { bestMatch, bestMatchIndex } = matches;
 
   return bestMatch.rating < 0.4 ? 'Desculpe, não entendi.' : botResponseData.outputs[bestMatchIndex];
